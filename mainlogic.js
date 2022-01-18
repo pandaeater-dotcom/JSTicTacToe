8ef327de5909fe36b17641a58fe5b3d11e7431d2
Rohit <r24rames@uwaterloo.ca>
2022-01-18 05:14:29 -0500

    logic done!

--- mainlogic.js
+++ mainlogic.js
@@ -27,9 +27,37 @@ function runGame(squareList) {
 function clicked(square, squareList) {
     if (square.classList.contains('checked')) return;
     square.classList.add('checked');
+    checkGameWon(whoseTurn, square, squareList);
+    square.setAttribute('data', whoseTurn);
     square.innerHTML = `<span>${whoseTurn}</span>`;
+    if (checkGameWon(whoseTurn, square, squareList)) {
+        gameOver = true;
+        togglePopup();
+        return;
+    }
     if (whoseTurn === 'X') whoseTurn = '〇';
     else whoseTurn = 'X';
+}
+
+function checkGameWon(whoseTurn, square, squareList) {
+    const index = parseInt(square.getAttribute('id').slice(1));
+    const leftEdge = index % 3 === 0;
+    const rightEdge = index % 3 === 1;
+    for (let i = 1; i <= 4; i++) if (!(leftEdge && i % 3 === 0) || !(rightEdge && i === 1)) {
+        if (squareList[index-i] && squareList[index-i].getAttribute('data') === whoseTurn && 
+            squareList[index+i] && squareList[index+i].getAttribute('data') === whoseTurn) return true;
+        else if (squareList[index-i] && squareList[index-i].getAttribute('data') === whoseTurn) {
+            if (squareList[index-2*i] && squareList[index-2*i].getAttribute('data') === whoseTurn) return true;
+        }
+        else if (squareList[index+i] && squareList[index+i].getAttribute('data') === whoseTurn) {
+            if (squareList[index+2*i] && squareList[index+2*i].getAttribute('data') === whoseTurn) return true;
+        }
+    }
+    return false;
+}
+
+function togglePopup() {
+    document.querySelector('.popup').classList.toggle('active');
 }
 
 runGame(initBoard());
